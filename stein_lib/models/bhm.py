cb844ef0d02a1cd81cf28c967fe5b2ffd2b4c5f7
Sasha <lambert.sasha@gmail.com>
2021-08-16 11:40:06 -0700

    fixed package name. Added to BHM map type

--- stein_lib/models/bhm.py
+++ stein_lib/models/bhm.py
@@ -1,16 +1,20 @@
 import numpy as np
 import torch
+from pathlib import Path
+from bhmlib.BHM.pytorch.bhmtorch_cpu import BHM2D_PYTORCH
 
 class BayesianHilbertMap:
-
     def __init__(
             self,
+            file_path=None,
     ):
 
-        self.dist = MixtureOfDiagNormals(mus, sigmas, mix_coeffs)
+        # Load trained Bayesian Hilbert Map
+        params = torch.load(file_path)
+        self.bhm = BHM2D_PYTORCH(**params)
 
     def log_prob(self, x):
-        return self.dist.log_prob(x)
+        return self.bhm.log_prob(x)
 
     def grad_log_p(self, x):
         x_ = torch.autograd.Variable(x, requires_grad=True)
@@ -19,3 +23,12 @@ class BayesianHilbertMap:
             x_,
         )[0]
         return dlog_p
+
+
+if __name__ == '__main__':
+
+    import bhmlib
+    bhm_path = Path(bhmlib.__path__[0]).resolve()
+    model_file = bhm_path / 'Outputs' / 'saved_models' / 'bhm_intel_res0.25_iter010.pt'
+
+    bhm = BayesianHilbertMap(model_file)
